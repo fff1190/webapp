1c0088694f7bca405e5df0747bccb7ec65d5092d
Fedor <fedor1990nik@gmail.com>
2025-01-07 13:33:25 +0500

    9 react and js

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
